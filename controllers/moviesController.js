a4c3507df57a0973753be6eb412b576beb86ec61
Delfi <villandelfi@gmail.com>
2020-05-13 17:09:50 -0300

    Plantilla personalizada para header y footer

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,15 +2,16 @@ const DB = require('../database/models');
 
 const controller={
 store: (req, res)=>{
-    db. //NOMBRE DEL MODELO DE LA DB DE USUARIOS (USERS)
-    .create(req.body)
+    db
+     //NOMBRE DEL MODELO DE LA DB DE USUARIOS (USERS)
+    //.create(req.body)
 
-    .then(userCreado =>{
-        return res.render('/')
-    })
-    .catch(function(error){
-        return res.send(error)
-    })
+    //.then(userCreado =>{
+    //    return res.render('/')
+    //})
+    //.catch(function(error){
+   //     return res.send(error)
+  //  })
 }
 
 
