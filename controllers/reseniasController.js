bfd68ce2cc088322f85c0f8ad593315893193c9b
esperanza uranga <euranga@udesa.edu.ar>
2020-05-23 19:15:21 -0300

    un poquito de reseñas

---
+++ controllers/reseniasController.js
@@ -0,0 +1,6 @@
+// const DB = require('../database/models');
+// const OP = DB.Sequelize.Op;
+
+
+
+// module.exports = controller;
---
+++ routes/resenias.js
@@ -0,0 +1,11 @@
+// var express = require('express');
+// var router = express.Router();
+// const controller = require('../controllers/reseniasController');
+
+// router.get('/', function(req, res) {
+//     res.render('resenias');
+//   });
+
+
+
+// module.exports = router;
