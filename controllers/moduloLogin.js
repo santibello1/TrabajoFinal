351342631e572a2676a31d668914484846503a3b
Delfi <villandelfi@gmail.com>
2020-05-27 11:41:22 -0300

    modulo log in del playground en el proyecto para requerir

---
+++ controllers/moduloLogin.js
@@ -0,0 +1,40 @@
+let db = require('./database/models')
+
+let moduloLogin = {
+    chequearUsuario: function (email) {
+        return db.Usuarios.findOne({
+            where: {
+                email: email
+            }
+        })
+        .then(function(usuario) {
+            return usuario != null;
+        })
+    },
+
+    buscarPorEmail: function (email){
+        return db.Usuarios.findOne({
+            where: {
+                email:email
+            }
+        })
+        .then(resultado=> {
+            return resultado
+        })
+    },
+
+    validar: function (email, pass) {
+        return db.Usuarios.findOne({
+            where:{
+                email:email,
+                password: password
+            },
+        })
+        .then(results=>{
+            return results;
+        })
+    }
+}
+
+
+module.exports = moduloLogin;
--- controllers/reseniasController.js
+++ controllers/reseniasController.js
@@ -27,40 +27,6 @@ const controller = {
             })
      },
      // guardar la resenia en la db. 
-     // COMO HAGO PARA HACER CON LA INFO DE LA DB DE USUARIOS CREAR ALGO EN OTRA DB
-     chequearUsuario: function (email) {
-        return db.Usuario.findOne({
-            where: {
-                email: email
-            }
-        })
-        .then(function(usuario) {
-            return usuario != null;
-        })
-    },
-
-    buscarPorEmail: function (email){
-        return db.Usuario.findOne({
-            where: {
-                email:email
-            }
-        })
-        .then(resultado=> {
-            return resultado
-        })
-    },
-
-    validar: function (email, password) {
-        return db.Usuario.findOne({
-            where:{
-                email:email,
-                password: password
-            },
-        })
-        .then(results=>{
-            return results;
-        })
-    },
     store: function (req,res){
         DB.Resenias
         .findOne({
